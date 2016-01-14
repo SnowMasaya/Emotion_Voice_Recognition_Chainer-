1372a26ecc2ac6b828f2f85b02eec2fbe7a17104
SnowMasaya <snow.akogi.pgel@gmail.com>
2016-01-14 09:24:29 +0900

    delete

--- make_arff/make_arff.py
+++ make_arff/make_arff.py
@@ -5,18 +5,20 @@ import os
 
 class Make_arff():
 
-    def __init__(self, wav_list):
+    def __init__(self, wav_list, config_file, output_dir):
         self.wav_list = wav_list
         self.smile_extract = os.environ["OPENSMILE_PATH"]
-        self.conf_file = os.environ["OPENSMILE_CONFIG"]
-        self.output_dir = os.environ["OPENSMILE_OUTPUT"]
+        self.config_file = config_file
+        self.output_dir = output_dir
 
     def extract(self):
+        print(self.config_file)
+        print(self.output_dir)
         f = open(self.wav_list, 'r')
         for line in f:
             wav_list = line.replace("\n", "")
             wav_list_split = line.replace("\n", "").split("/")
             output_files = self.output_dir + wav_list_split[len(wav_list_split) - 1].replace("wav", "arff")
-            extract_code = "%s -C %s -I %s -O %s" % (self.smile_extract, self.conf_file, wav_list, output_files)
+            extract_code = "%s -C %s -I %s -O %s" % (self.smile_extract, self.config_file, wav_list, output_files)
             print(extract_code)
             os.system(extract_code)
--- make_arff/test/test_arff.py
+++ make_arff/test/test_arff.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import unittest
+from argparse import ArgumentParser
 import os
 import sys
 #you set the script path for input_anvil parsing_anvil
@@ -9,15 +9,47 @@ sys.path.append(os.path.join(os.path.dirname(__file__), "../"))
 from make_arff import Make_arff
 
 
-class TestSequenceFunctions(unittest.TestCase):
+"""Applies forward propagation with chaining backward references.
+   Basic behavior is expressed in documentation of :class:`Function`
+   class.
+   .. note::
+   If the :data:`~Variable.data` attribute of input variables exist on
+   GPU device, then, before it calls :meth:`forward` method, the
+   appropriate device is selected, so in most cases implementers do
+   not need to take care of device selection.
+   Args:
+       inputs: Tuple of input :class:`Variable` objects. The volatile
+           flags of all input variables must agree.
+   Returns:
+       One :class:`Variable` object or a tuple of multiple
+       :class:`Variable` objects.
+"""
 
-    def setUp(self):
-        print("set up")
+def parse_args():
+    p = ArgumentParser(description='Choose Make arff output config')
 
-    def test_input(self):
-        data_path = os.environ["OPENSMILE_PYTHON"]
-        make_arff = Make_arff(data_path + "/wav_list")
-        make_arff.extract()
+    p.add_argument('-c', '--config', type=str, help='\'OPENSMILE_CONFIG\' or \'OPENSMILE_CONFIG_2010\'')
+    p.add_argument('-o', '--output_dir', default='output',type=str, help='[output dir]  you set output dir')
+    args = p.parse_args()
+
+    # check args
+    try:
+        if args.config not in ['OPENSMILE_CONFIG', 'OPENSMILE_CONFIG_2010']: raise ValueError('you must set mode = \'OPENSMILE_CONFIG\' or \'OPENSMILE_CONFIG_2010\'')
+    except Exception as ex:
+        p.print_usage(file=sys.stderr)
+        print(ex)
+        print(sys.stderr)
+        sys.exit()
+
+    return args
+
+def test_input():
+    args = parse_args()
+    data_path = os.environ["OPENSMILE_PYTHON"]
+    config_file = os.environ[args.config]
+    output_dir = args.output_dir
+    make_arff = Make_arff(data_path + "/wav_list", config_file, output_dir)
+    make_arff.extract()
 
 if __name__ == '__main__':
-    unittest.main()
+    test_input()
